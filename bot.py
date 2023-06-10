5e602dabc62346cb8ad25577e3a237759447b195
Kiki2356 <49116646+Kiki2356@users.noreply.github.com>
2023-06-11 01:56:24 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
+from flask import Flask, render_template
 import logging
-from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from fpdf import FPDF
+import os
+
+app = Flask(__name__)
+bot_running = False
 
 # Fungsi untuk mengolah file PDF dan membuat kesimpulan jurnal
-def handle_upload_pdf(update: Update, context):
+def handle_upload_pdf(update, context):
     # Periksa apakah file PDF telah dikirimkan
     if not update.message.document:
         update.message.reply_text('Mohon unggah file PDF.')
@@ -29,12 +33,17 @@ def handle_upload_pdf(update: Update, context):
     # Kirim PDF hasil kesimpulan ke pengguna
     context.bot.send_document(chat_id=update.effective_chat.id, document=open("output.pdf", "rb"))
 
-def main():
-    # Konfigurasi logging
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+def start(update, context):
+    global bot_running
+    bot_running = True
+    update.message.reply_text('Bot Telegram telah diaktifkan.')
 
-    # Buat objek updater dan pengendali
-    updater = Updater("5649401123:AAHs97ocsxdZIm9Uc4tXsZZk8t68WxsodMw", use_context=True)
+def run_bot():
+    # Dapatkan token akses bot dari variabel lingkungan
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
+
+    # Buat objek updater dan pengendali dengan token akses
+    updater = Updater(token, use_context=True)
     dp = updater.dispatcher
 
     # Daftarkan handler perintah /start
@@ -49,5 +58,10 @@ def main():
     # Matikan bot saat ditekan Ctrl+C
     updater.idle()
 
+@app.route('/')
+def index():
+    return render_template('index.html', bot_running=bot_running)
+
 if __name__ == '__main__':
-    main()
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
